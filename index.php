8889f4f9371c4ad7fc0bffac42c4bc68a8cc95b0
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-03-12 15:28:01 -0300

    nav finalizada

--- index.php
+++ index.php
@@ -1,41 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Mnu principal</title>
+    
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>Menu principal</title>
 </head>
+
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">MENU</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Multa</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Números primos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Temperatura</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">IMC</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <nav>
+            <h1>MENU</h1>
+            <div id="divNav1">
+                <ul id="linksPages">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="pages/multa.php">Multa</a></li>
+                    <li><a href="pages/primos.php">Primos</a></li>
+                    <li><a href="pages/imc.php">IMC</a></li>
+                    <li><a href="pages/temperaturas.php">Temperaturas</a></li>
+
+                </ul>
+                <ul id="linksDev">
+                    <li><a href="https://github.com/carlos-verdeiro" target="_blank"><?php include "assets/img/github.svg"?></a></li>
+                    <li><a href="https://www.linkedin.com/in/carlos-daniel-verdeiro/" target="_blank"><?php include "assets/img/linkedin.svg"?></a></li>
+                    <li><a href="https://carlos-verdeiro.github.io/" target="_blank"><?php include "assets/img/web.svg"?></a></li>
+                </ul>
+            </div>
+        </nav>
     </header>
 </body>
+
 </html>
