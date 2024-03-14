955ba4f0855c83282907beb0f9d532e31a94eb31
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-03-14 20:44:56 -0300

    início resposta IMC

--- pages/imc.php
+++ pages/imc.php
@@ -1,3 +1,20 @@
+<?php
+$respondido = false;
+if (isset($_GET['peso']) || isset($_GET['altura'])) {
+    if (isset($_GET['peso'])) {
+        $peso = $_GET['peso'];
+    }
+    if (isset($_GET['altura'])) {
+        $altura = $_GET['altura'];
+    }
+    if (isset($_GET['peso']) && isset($_GET['altura'])) {
+        $imc = $peso / ($altura*$altura);
+        $respondido = true;
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -33,14 +50,34 @@
             <form action="imc.php" method="get" class="form">
                 <div class="inputContainer" id="iCPeso">
                     <label for="peso">Peso:</label>
-                    <input placeholder="Kg" type="number" name="peso" id="peso" min="0.1 " max required>
+                    <input placeholder="Kg" type="number" step="0.01" name="peso" id="peso" min="0.1 " max required>
                 </div>
                 <div class="inputContainer" id="iCAltura">
                     <label for="altura">Altura:</label>
-                    <input placeholder="Metro" type="number" name="altura" id="altura" min="0.1 " required>
+                    <input placeholder="Metro" type="number" step="0.01" name="altura" id="altura" min="    1 " required>
                 </div>
                 <input type="submit" value="Calcular" class="submit">
             </form>
+        </section>
+        <section class="section">
+            <table class="tableImc">
+                <thead>
+                    <th>Baixo Peso</th>
+                    <th>Peso Ideal</th>
+                    <th>Levemente acima do Peso</th>
+                    <th>Obesidade I</th>
+                    <th>Obesidade II</th>
+                    <th>Obesidade III</th>
+                </thead>
+                <tbody>
+                    <td><18,5</td>
+                    <td>18,5 - 24,9</td>
+                    <td>25 - 29,9</td>
+                    <td>30 - 34,9</td>
+                    <td>35 - 39,9</td>
+                    <td>>40</td>
+                </tbody>
+            </table>
         </section>
     </main>
     <?php include_once "../templates/footer.php"?>
